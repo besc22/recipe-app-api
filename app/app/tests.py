9cf91e527fbb699a33cf01eec5b2ae4bb97e2673
besc22 <beni.schmidt.2002@gmail.com>
2025-08-08 10:24:42 +0200

    mini test

--- app/app/tests.py
+++ app/app/tests.py
@@ -13,4 +13,10 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """Test substracting numbers."""
+        res = calc.substract(10, 15)
+        
+        self.assertEqual(res, 5)
         
