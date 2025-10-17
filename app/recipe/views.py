29adb86506193ead30af4804e49647065de8d3a3
besc22 <beni.schmidt.2002@gmail.com>
2025-10-17 12:39:10 +0200

    implemented tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,11 +4,17 @@ Define all the HTTP requests that can be made to the recipe endpoints (GET, POST
 Controls what data can be seen and manipulated based on the serializer definitions.
 Implement authentication and permissions to ensure only authorized users can access their data.
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
 from recipe import serializers
 
 
@@ -34,3 +40,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet
+                 ):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
